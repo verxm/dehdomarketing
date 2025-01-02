71481906cbcd277d79aaa50bc7ea56c7e4eb4974
Davi C Santos <davisantos448@gmail.com>
2025-01-02 18:36:11 -0300

    feat(layout.tsx): change title and description of metadata.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  darkMode: ["class", "class"],
+  darkMode: ["class"],
   theme: {
   	extend: {
   		colors: {

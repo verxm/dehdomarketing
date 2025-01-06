dd2800a35ad80875c68669002d862ffb8593d305
Davi C Santos <davisantos448@gmail.com>
2025-01-05 23:15:14 -0300

    style(Contacts): adds transition to button hover.

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -6,15 +6,30 @@ import { FiMail } from "react-icons/fi";
 const Contacts = () => {
     return (
         <div className="flex flex-row justify-center gap-3 md:gap-5">
-            <Button variant="default" className="border border-white shadow-primary shadow-md w-12 h-12 rounded-full hover:shadow-sm">
+            <Button variant="default" className="
+                border border-white 
+                shadow-primary shadow-md 
+                w-12 h-12
+                rounded-full 
+                hover:shadow-sm hover:transition-all">
                 <BsWhatsapp size={64}/>
             </Button>
 
-            <Button variant="default" className="border border-white shadow-primary shadow-md w-12 h-12 rounded-full hover:shadow-sm">
+            <Button variant="default" className="
+                border border-white
+                shadow-primary shadow-md 
+                w-12 h-12 
+                rounded-full 
+                hover:shadow-sm hover:transition-all">
                 <BsInstagram size={64}/>
             </Button>
 
-            <Button variant="default" className="border border-white shadow-primary shadow-md w-12 h-12 rounded-full hover:shadow-sm">
+            <Button variant="default" className="
+                border border-white 
+                shadow-primary shadow-md 
+                w-12 h-12 
+                rounded-full 
+                hover:shadow-sm hover:transition-all">
                 <FiMail size={64}/>
             </Button>
         </div>

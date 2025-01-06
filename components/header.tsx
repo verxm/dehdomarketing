ecc1902e727f450f549cb36ae0a7882d555626b8
Davi C Santos <davisantos448@gmail.com>
2025-01-05 21:28:50 -0300

    feat(Header): add get started button.

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,20 @@
 import Link from "next/link";
 import { Card, CardContent } from "./ui/card";
 import Image from "next/image";
+import { Button } from "./ui/button";
 
 const Header = () => {
     return (
         <header>
             <Card className="rounded-t-none">
-                <CardContent className="p-2 justify-between flex flex-row ml-2">
+                <CardContent className="p-2 justify-between flex flex-row ml-2 items-center">
                     <Link href="/">
                         <Image src="/logo-sem-fundo-marrom.png" alt="Deh do Marketing" height={100} width={70} />
                     </Link>
+
+                            <Button className="font-bold rounded-full mr-3" size="sm">
+                                Começar
+                            </Button>
                 </CardContent>
             </Card>
         </header>

6f386793af3d0266b29314312bdf50acf79d352c
Davi C Santos <davisantos448@gmail.com>
2025-01-07 17:42:56 -0300

    refactor(Contacts): reorganize elements - remove blank lines.

--- components/contacts.tsx
+++ components/contacts.tsx
@@ -4,9 +4,7 @@ const Contacts = () => {
     return (
         <div className="flex flex-row justify-center gap-3 md:gap-5">
             <ContactButton contactType={ContactType.WhatsApp} />
-
             <ContactButton contactType={ContactType.Instagram} />
-
             <ContactButton contactType={ContactType.Email} />
         </div>
     );

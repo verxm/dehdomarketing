51fa2fc453ea8822ea9849d34222f543aed5ea27
Davi C Santos <davisantos448@gmail.com>
2025-01-07 17:27:49 -0300

    feat(ContactButton): add tooltip.

--- components/contact-button.tsx
+++ components/contact-button.tsx
@@ -1,6 +1,7 @@
 import { BsInstagram, BsWhatsapp } from "react-icons/bs";
 import { Button } from "./ui/button";
 import { FiMail } from "react-icons/fi";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 interface ContactButtonParameters {
     contactType: ContactType,
@@ -14,30 +15,45 @@ export enum ContactType {
 
 interface ContactButtonProperties {
     Icon: JSX.Element,
+    Tooltip: string,
 }
 
 const ContacButton = ({ contactType }: ContactButtonParameters) => {
     const buttonProperties: { [key in ContactType]?: ContactButtonProperties } = {
         [ContactType.WhatsApp]: {
             Icon: <BsWhatsapp size={64} />,
+            Tooltip: "+55 51 99902-8748",
         },
         [ContactType.Instagram]: {
             Icon: <BsInstagram size={64} />,
+            Tooltip: "@dehdomarketing",
         },
         [ContactType.Email]: {
-            Icon: <FiMail size={64} />
+            Icon: <FiMail size={64} />,
+            Tooltip: "contatodeboradeborba@gmail.com",
         }
     }
 
     return (
-        <Button variant="default" className="
-                border border-white 
-                shadow-primary shadow-md 
-                w-12 h-12
-                rounded-full 
-                hover:shadow-sm hover:transition-all">
-            {buttonProperties[contactType]?.Icon}
-        </Button>
+        // TODO: ver se ela gosta do tooltipe para mobile - opição: popover
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    <Button variant="default" className="
+                            border border-white 
+                            shadow-primary shadow-md 
+                            w-12 h-12
+                            rounded-full 
+                            hover:shadow-sm hover:transition-all">
+                        {buttonProperties[contactType]?.Icon}
+                    </Button>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                    <p>{buttonProperties[contactType]?.Tooltip}</p>
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+
     )
 }
 

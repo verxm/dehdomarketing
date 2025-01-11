f121637eb71a8de6a5715786bf6b3794b398bf2a
Davi <40809513+verxm@users.noreply.github.com>
2025-01-11 16:53:41 -0300

    feat: implements the initial version of the ServiceItem component.
    ***
    
    ## Release Notes
    
    ### Overview
    
    The component was initially created using one of the Acertanity UI cards, but the experiment did not prove to be very pleasant, so the main component used in `ServiceItem` was the `Card` from shadcn itself.
    
    ### Purpose
    
    The initial component was created to be displayed on the application's home screen, making it easier to read and reuse.
    
    ### Notes
    
    The `LineBreakText` helper component was created to be used to break lines of strings.
    
    ***

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import Header from "@/components/header";
 import PresentationSession from "./presentation-session";
 import Contacts from "@/components/contacts";
+import ServiceItem from "@/components/service-item";
 
 export default function Home() {
   return (
@@ -13,6 +14,16 @@ export default function Home() {
         <div className="pt-5">
           <Contacts />
         </div>
+
+        <ServiceItem
+          name="Gestão de Redes Sociais"
+          description="▸ Planejamento e desenvolvimento de conteúdos para as redes sociais\n
+            ▸ Desenvolvimento de banners e imagens\n
+            ▸ Roteiro para stories e vídeos\n
+            ▸ Criação de legendas persuasivas\n
+            ▸ Captação e edição de vídeos\n
+            ▸ Briefing, análise de dados para entender melhor a necessidade do público alvo"
+          imagePath="/gestao-de-redes-sociais.jpeg" />
       </div>
     </>
   );
---
+++ components/service-item.tsx
@@ -0,0 +1,43 @@
+import LineBreakText from "./line-break-text";
+import { Button } from "./ui/button";
+import { Card, CardContent, CardFooter } from "./ui/card";
+import Image from "next/image";
+
+interface ServiceItemParameters {
+    name: string,
+    description: string,
+    imagePath: string,
+}
+
+const ServiceItem = ({ name, description, imagePath }: ServiceItemParameters) => {
+    return (
+        <>
+            <div className="py-5">
+                <Card className="min-w-full max-w-full rounded-2xl">
+                    <CardContent className="px-2 py-2">
+                        <div className="relative w-full h-[300px]">
+                            <Image
+                                src={imagePath}
+                                alt={name}
+                                style={{
+                                    objectFit: "cover"
+                                }}
+                                fill
+                                className="rounded-2xl" />
+                        </div>
+
+                        <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{name}</h2>
+                        <div className="pl-2 pt-2 text-sm text-muted-foreground">
+                            <LineBreakText text={description} lineClassName="line-clamp-1" maxLinesNumber={4} />
+                        </div>
+                    </CardContent>
+                    <CardFooter className="flex justify-end p-5 pt-1">
+                        <Button variant="outline">Ver mais</Button>
+                    </CardFooter>
+                </Card>
+            </div>
+        </>
+    );
+}
+
+export default ServiceItem;

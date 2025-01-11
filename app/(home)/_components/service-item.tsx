a5e1d7ed4c61b409c9a407c6a2d2998d624a40fe
Davi C Santos <davisantos448@gmail.com>
2025-01-11 18:18:00 -0300

    chore(ServiceItem): remove unnecessary div.

--- app/(home)/_components/service-item.tsx
+++ app/(home)/_components/service-item.tsx
@@ -11,32 +11,28 @@ interface ServiceItemParameters {
 
 const ServiceItem = ({ name, description, imagePath }: ServiceItemParameters) => {
     return (
-        <>
-            <div className="py-5">
-                <Card className="min-w-full max-w-full rounded-2xl">
-                    <CardContent className="px-2 py-2">
-                        <div className="relative w-full h-[300px]">
-                            <Image
-                                src={imagePath}
-                                alt={name}
-                                style={{
-                                    objectFit: "cover"
-                                }}
-                                fill
-                                className="rounded-2xl" />
-                        </div>
+        <Card className="min-w-full max-w-full rounded-2xl">
+            <CardContent className="px-2 py-2">
+                <div className="relative w-full h-[300px]">
+                    <Image
+                        src={imagePath}
+                        alt={name}
+                        style={{
+                            objectFit: "cover"
+                        }}
+                        fill
+                        className="rounded-2xl" />
+                </div>
 
-                        <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{name}</h2>
-                        <div className="pl-2 pt-2 text-sm text-muted-foreground">
-                            <LineBreakText text={description} lineClassName="line-clamp-1" maxLinesNumber={4} />
-                        </div>
-                    </CardContent>
-                    <CardFooter className="flex justify-end p-5 pt-1">
-                        <Button variant="outline">Ver mais</Button>
-                    </CardFooter>
-                </Card>
-            </div>
-        </>
+                <h2 className="font-bold mt-2 overflow-hidden text-ellipsis text-nowrap">{name}</h2>
+                <div className="pl-2 pt-2 text-sm text-muted-foreground">
+                    <LineBreakText text={description} lineClassName="line-clamp-1" maxLinesNumber={4} />
+                </div>
+            </CardContent>
+            <CardFooter className="flex justify-end p-5 pt-1">
+                <Button variant="outline">Ver mais</Button>
+            </CardFooter>
+        </Card>
     );
 }
 

2a045dc51d721454860310801ec14a41558f2252
Davi C Santos <davisantos448@gmail.com>
2025-01-12 16:59:04 -0300

    style(Header): fixes the header.

--- components/header.tsx
+++ components/header.tsx
@@ -5,8 +5,8 @@ import { Button } from "./ui/button";
 
 const Header = () => {
     return (
-        <header className="mt-[-1px] mr-[-1px] ml-[-1px]">
-            <Card className="rounded-t-none border border-primary border-solid shadow-md">
+        <header className="mt-[-1px] mr-[-1px] ml-[-1px] sticky top-0 z-20">
+            <Card className="rounded-t-none border-t-0 border-r-0 border-l-1 border-b-1 border-primary border-solid shadow-md">
                 <CardContent className="p-2 justify-between flex flex-row ml-2 items-center">
                     <Link href="/">
                         <Image src="/logo-sem-fundo-marrom.png" alt="Deh do Marketing" height={100} width={70} />

3a0e6783f4520f5f353a1617037c549c51175994
Davi C Santos <davisantos448@gmail.com>
2025-01-14 14:09:30 -0300

    chore(FeedbackSession): remove unecessary divs.

--- app/(home)/_components/feedback-session.tsx
+++ app/(home)/_components/feedback-session.tsx
@@ -15,16 +15,12 @@ const FeedbackSession = () => {
             <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
                 <Marquee pauseOnHover className="[--duration:60s]">
                     {allFeedbacks.map((feedback) => (
-                        <div className="max-w-[390px] h-[320px]">
-                            <FeedbackItem feedback={feedback} />
-                        </div>
+                        <FeedbackItem feedback={feedback} />
                     ))}
                 </Marquee>
                 <Marquee reverse pauseOnHover className="[--duration:60s]">
                     {allFeedbacks.map((feedback) => (
-                        <div className="max-w-[390px]">
-                            <FeedbackItem feedback={feedback} />
-                        </div>
+                        <FeedbackItem feedback={feedback} />
                     ))}
                 </Marquee>
                 <div className="pointer-events-none absolute inset-y-0 left-0 w-1/5 bg-gradient-to-r from-white"></div>

dca2a9910921921a0b3d89bb884f91b1c4cd9e56
Davi C Santos <davisantos448@gmail.com>
2025-01-15 10:27:00 -0300

    refactor(InstagramInsigths): parameterizes variant data and adds before and after Badge with range of dates displayed in Popover.

--- app/(home)/_components/customer-item.tsx
+++ app/(home)/_components/customer-item.tsx
@@ -4,26 +4,41 @@ import InstagramInsights from "./instagram-insigths";
 
 const CustomerItem = () => {
     return (
-        <Card className="mt-14 border-gray-400">
+        <Card className="mt-14 border-primary shadow-none">
             <CardContent className="p-0">
                 <div className="flex flex-col">
                     <div className="items-center flex flex-col pt-5">
                         <Image
                             className="
-                                rounded-full border border-gray-400
+                                rounded-full border border-primary
                                 absolute 
                                 mt-[-105px]
                                 object-cover"
                             src="/derma-locacoes-logo.PNG"
                             alt="Logo Deh do Marketing" height={180} width={180} />
                     </div>
-                    <div className="pt-24 px-5 pb-7 border-b border-gray-400">
+                    <div className="pt-24 px-5 pb-7 border-b border-primary">
                         <h3 className="font-bold">Derma Locações</h3>
                         <p className="text-sm text-gray-500">Locações de equipamentos estéticos</p>
                     </div>
-                    <div className="px-5 pt-5">
-                        <InstagramInsights />
+                    <div className="px-5 pt-5 border-b border-dashed border-primary">
+                        <InstagramInsights
+                            insigthsDateRange="09/01/2024 a 09/02/2024"
+                            accountsReached="10"
+                            accountsWithEngagement="200"
+                            totalFollowers="5"
+                            sharedContents="20"
+                            isBefore={true} />
                     </div>
+                    <div className="px-5 pt-5 border-b border-primary">
+                        <InstagramInsights
+                            insigthsDateRange="04/11/2024 a 04/12/2024"
+                            accountsReached="10"
+                            accountsWithEngagement="200"
+                            totalFollowers="5"
+                            sharedContents="20" />
+                    </div>
+                    <p className="p-5"> Em breve...</p>
                 </div>
             </CardContent>
         </Card>
--- app/(home)/_components/instagram-insigths.tsx
+++ app/(home)/_components/instagram-insigths.tsx
@@ -1,22 +1,66 @@
-const InstagramInsights = () => {
+import { Badge } from "@/components/ui/badge";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+interface InstagramInsightsParameters {
+    insigthsDateRange: string,
+    accountsReached: string,
+    accountsWithEngagement: string,
+    totalFollowers: string,
+    sharedContents: string,
+    isBefore?: boolean | undefined,
+}
+
+const InstagramInsights = ({
+    insigthsDateRange,
+    accountsReached,
+    accountsWithEngagement,
+    totalFollowers,
+    sharedContents,
+    isBefore = false }: InstagramInsightsParameters) => {
+    const InsigthsBadge = () => {
+        if (isBefore) {
+            return <Badge variant="secondary" className="text-primary">Antes</Badge>
+        }
+
+        return <Badge>Depois</Badge>
+    }
+
+    const InsigthPopoverStyle = () => {
+        if (isBefore) {
+            return "bg-muted text-primary"
+        }
+
+        return "bg-primary text-white";
+    }
+
     return (
         <>
             <div className="flex flex-row justify-between">
                 <p className="font-bold text-sm">Insigths</p>
-                <p className="text-xs italic">09/01/2024 a 09/02/2024</p>
+                {/* <p className="text-xs italic text-gray-600 pt-[3px]">{insigthsDateRange}</p> */}
+                <Popover >
+                    <PopoverTrigger asChild>
+                    <InsigthsBadge />
+                    </PopoverTrigger>
+                    <PopoverContent className= {`w-full h-7 p-0 px-2 items-center justify-center flex flex-col ${InsigthPopoverStyle()}`}  >
+                    <p className="text-xs">{insigthsDateRange}</p>
+                    </PopoverContent>
+                </Popover>
+
             </div>
-            <div className="flex flex-row p-5">
-                <div className="flex flex-col w-[270px] text-gray-600 text-sm">
+            <div className="flex flex-row justify-between p-4">
+                <div className="flex flex-col text-sm">
                     <p>Contas alcançadas:</p>
                     <p>Contas com engajamento:</p>
                     <p>Total de seguidores:</p>
                     <p>Conteúdo que você compartilhou:</p>
                 </div>
                 <div className="flex flex-col text-sm">
-                    <p>10</p>
-                    <p>200</p>
-                    <p>5400</p>
-                    <p>20</p>
+                    <p>{accountsReached}</p>
+                    <p>{accountsWithEngagement}</p>
+                    <p>{totalFollowers}</p>
+                    <p>{sharedContents}</p>
                 </div>
             </div>
         </>

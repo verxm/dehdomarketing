b2b6ed426178310a174ca1d26ae0c873034304a8
Davi C Santos <davisantos448@gmail.com>
2025-01-27 21:51:49 -0300

    feat: implements initial WorkFormItem component.

---
+++ app/(home)/_components/work-form-item.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
+
+interface WorkFormParameters {
+    imagePath: string,
+    title: string,
+    description: string,
+}
+
+const WorkFormItem = ({ imagePath, title, description }: WorkFormParameters) => {
+    return (
+        <Card className="min-w-full max-w-full rounded-2xl">
+            <CardContent className="p-0">
+                <div className="
+                  flex flex-col items-center 
+                  h-20
+                  rounded-2xl bg-primary
+                  rounded-b-none">
+                    <Image
+                        src={imagePath}
+                        alt={title}
+                        className="rounded-full object-cover absolute h-[150px] mt-2"
+                        height={150} width={150} />
+                </div>
+
+                <div className="px-4 pt-20 rounded-2xl">
+                    <h3 className="font-bold mt-2 overflow-hidden text-primary font-tanpearl text-ellipsis text-nowrap">{title}</h3>
+                    <p className="pl-3 pt-3 text-sm min-h-[90px] max-h-[90px] whitespace-pre-line line-clamp-4">
+                        {description}
+                    </p>
+                </div>
+
+            </CardContent>
+        </Card>
+    );
+}
+
+export default WorkFormItem;

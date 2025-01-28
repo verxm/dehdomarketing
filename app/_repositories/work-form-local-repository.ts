4e3c0bbd10f4b3e34f05b24ff10a63da2af65178
Davi C Santos <davisantos448@gmail.com>
2025-01-28 20:50:38 -0300

    chore: change work form images.

--- app/_repositories/work-form-local-repository.ts
+++ app/_repositories/work-form-local-repository.ts
@@ -5,25 +5,25 @@ const allWorkForms = [
         id: "1",
         title: "Planejamento",
         description: "Vou entender mais sobre o seu negócio, elaborar um bom plano visando alcançar os seus objetivos.",
-        imagePath: "/gestao-de-redes-sociais.jpeg",
+        imagePath: "/work-form-planejamento.PNG",
     },
-    { // TODO: alterar image paths
+    {
         id: "2",
         title: "Execução",
         description: "Utilizo as melhores ferramentas disponíveis para ir além das expectativas e aumentar suas vendas.",
-        imagePath: "/trafego-page.png",
+        imagePath: "/work-form-execucao.PNG",
     },
     {
         id: "3",
         title: "Conversação",
         description: "Alguém está procurando pelo seu serviço neste momento. Seja encontrado antes da concorrência.",
-        imagePath: "/analise-de-perfil.jpeg",
+        imagePath: "/work-form-conversao.PNG",
     },
     {
         id: "4",
         title: "Relacionamento",
         description: "Sua empresa marcará presença na internet, não só para ganhar likes, mas sim para ser reconhecido.",
-        imagePath: "/prod-conteudo.jpeg",
+        imagePath: "/work-form-relacionamento.JPG",
     },
 ] as WorkForm[];
 

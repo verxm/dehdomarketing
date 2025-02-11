806bafddf9dc1556d751db7e425ef9e22ae9244f
Davi C Santos <davisantos448@gmail.com>
2025-02-10 21:33:32 -0300

    style(ScrollDownIndicator): repositions arrow and makes it opaque.

--- components/scroll-down-indicator.tsx
+++ components/scroll-down-indicator.tsx
@@ -2,8 +2,8 @@ import { FaLongArrowAltDown } from "react-icons/fa";
 
 const ScrollDownIndicator = () => {
     return (
-        <div className="flex flex-row justify-end pb-3">
-            <FaLongArrowAltDown className="absolute text-gray-500" />
+        <div className="flex flex-row justify-end pb-5 mt-[-18px] pr-2">
+            <FaLongArrowAltDown className="absolute text-gray-500 opacity-60" />
         </div>
     );
 }

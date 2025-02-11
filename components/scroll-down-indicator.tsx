44dfa7f29ff9bb6ae1f49c671c27fedb7cade720
Davi C Santos <davisantos448@gmail.com>
2025-02-10 22:36:09 -0300

    feat(ScrollDownIndicator): animate arrow down icon.

--- components/scroll-down-indicator.tsx
+++ components/scroll-down-indicator.tsx
@@ -1,9 +1,20 @@
+'use client'
+
+import { motion } from "framer-motion";
 import { FaLongArrowAltDown } from "react-icons/fa";
 
 const ScrollDownIndicator = () => {
     return (
-        <div className="flex flex-row justify-end pb-5 mt-[-18px] pr-2">
-            <FaLongArrowAltDown className="absolute text-gray-500 opacity-60" />
+        <div className="flex flex-row justify-end pb-5 mt-[-18px] pr-6">
+            <motion.div animate={{
+                y: [0, 5, 0]
+            }}
+            transition={{
+                duration: 1.5,
+                repeat: Infinity,
+            }}>
+                <FaLongArrowAltDown className="absolute text-gray-500 opacity-60" />
+            </motion.div>
         </div>
     );
 }

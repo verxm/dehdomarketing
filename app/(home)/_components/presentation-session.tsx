28a5cd22fcaa4320252775a11878c6535e919fb3
Davi Santos <davisantos448@gmail.com>
2025-02-11 21:50:39 -0300

    feat(PresentationSession): make image animation responsive.

--- app/(home)/_components/presentation-session.tsx
+++ app/(home)/_components/presentation-session.tsx
@@ -8,23 +8,28 @@ const PresentationSession = () => {
     const words = ["propósito", "estratégia", "qualidade", "segurança"];
 
     return (
-        <div className="flex flex-col justify-center items-center text-center mx-auto">
+        <div className="flex flex-col justify-center items-center text-center">
             <div className="flex flex-col items-center justify-center">
-                <div className="
-                    w-[400px] h-[400px]
-                    absolute mt-[200px]
-                    flex flex-col items-center justify-center">
-                    <motion.div
-                        initial={{ width: "100%", height: "100%", y: 150 }}
-                        animate={{ width: "50%", height: "50%", y: 0 }}
-                        transition={{ duration: 2, ease: "easeInOut" }}
-                        className="
-                            border-x-4 border-white bg-primary 
-                            rounded-full shadow-md
-                            ">
-                        <Image src="/logo.png" alt="Logo Deh do Marketing" className="h-full w-full object-cover" width={400} height={400} />
-                    </motion.div>
-                </div>
+                <motion.div
+                    initial={{
+                         width: "var(--width-from, 550px)", 
+                         height: "var(--height-from, 550px)", 
+                         y: 150
+                    }}
+                    animate={{ 
+                        width: "var(--width-to)", 
+                        height: "var(--height-to)", 
+                        y: 0
+                    }}
+                    transition={{ duration: 2, ease: "easeInOut" }}
+                    className="
+                        border-x-4 border-white bg-primary 
+                        rounded-full shadow-md
+                        absolute mt-[200px]
+                        max-sm:[--width-from:400px] max-sm:[--height-from:400px]
+                        [--width-to:200px] [--height-to:200px]">
+                    <Image src="/logo.png" alt="Logo Deh do Marketing" className="h-full w-full object-cover" width={800} height={800} />
+                </motion.div>
             </div>
 
             <h1 className="font-[600] mt-[230px] text-3xl mb-4 tracking-tighter">

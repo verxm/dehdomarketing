5759a365cea3281998890f2ae8c527414411a6ab
Davi C Santos <davisantos448@gmail.com>
2025-02-18 19:37:18 -0300

    feat: animate ServiceSession.

--- app/(home)/_components/services-session.tsx
+++ app/(home)/_components/services-session.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { motion } from "framer-motion";
 import ServiceItem from "./service-item";
 import { servicesRepository } from "@/app/_repositories/services-local-repository";
 
@@ -5,7 +8,10 @@ const ServicesSession = () => {
     const services = servicesRepository.getAll();
 
     return (
-        <>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, transition: { duration: 1 } }}
+            viewport={{ once: true }}>
             <div className="px-5 mb-5 flex flex-col items-center">
                 <h2 className="text-lg uppercase text-primary font-bold">Serviços oferecidos</h2>
                 <p className="text-sm text-muted-foreground">Como podemos ajudar...</p>
@@ -17,12 +23,16 @@ const ServicesSession = () => {
                 overflow-x-auto [&::-webkit-scrollbar]:hidden
                 2xl:justify-center">
                 {services.map((service) => (
-                    <div key={service.id} className="min-w-[350px] max-w-[350px]">
+                    <motion.div 
+                        whileInView={{x: [100, 0], transition: {duration:0.5}}} 
+                        viewport={{once: true}}
+                        key={service.id} 
+                        className="min-w-[350px] max-w-[350px]">
                         <ServiceItem service={service} />
-                    </div>
+                    </motion.div>
                 ))}
             </div>
-        </>
+        </motion.div>
     );
 }
 

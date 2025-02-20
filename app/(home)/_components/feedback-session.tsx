ee980ac0a26b6401c780af4163bf4df4eeb3edd2
Davi C Santos <davisantos448@gmail.com>
2025-02-20 20:57:45 -0300

    feat: animate FeedbackSession.

--- app/(home)/_components/feedback-session.tsx
+++ app/(home)/_components/feedback-session.tsx
@@ -1,12 +1,18 @@
+'use client'
+
 import { feedbacksRepository } from "@/app/_repositories/feedbacks-local-repository";
 import FeedbackItem from "./feedback-item";
 import Marquee from "@/components/ui/marquee";
+import { motion } from "framer-motion";
 
 const FeedbackSession = () => {
     const allFeedbacks = feedbacksRepository.getAll();
 
     return (
-        <>
+        <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, transition: { duration: 1 } }}
+            viewport={{ once: true }}>
             <div className="px-5 flex flex-col items-center">
                 <h2 className="text-lg uppercase text-tertiary font-bold">✨ Feedbacks ✨</h2>
                 <p className="text-sm text-muted">De quem já vivieu essa transformação</p>
@@ -26,7 +32,7 @@ const FeedbackSession = () => {
                 <div className="pointer-events-none absolute inset-y-0 left-0 w-8 bg-gradient-to-r from-primary/50"></div>
                 <div className="pointer-events-none absolute inset-y-0 right-0 w-8 bg-gradient-to-l from-primary/50"></div>
             </div>
-        </>
+        </motion.div>
     );
 }
 

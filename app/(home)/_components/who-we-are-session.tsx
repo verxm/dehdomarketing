70a6bf0da3e1cb30e4129994704e24f49743693b
Davi C Santos <davisantos448@gmail.com>
2025-02-20 21:03:56 -0300

    feat: animate WhoWeAreSession.

--- app/(home)/_components/who-we-are-session.tsx
+++ app/(home)/_components/who-we-are-session.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import { Button } from "@/components/ui/button";
 import WhoWeAreImage from "./who-we-are-image";
 import { Key } from "lucide-react";
 import RequestBudget from "@/components/request-budget";
+import { motion } from "framer-motion";
 
 const WhoWeAreSession = () => {
     const description = "Oi, eu sou a Déh do Marketing! 🚀\n\nDesde 2019, venho ajudando empresas a se posicionarem no digital de forma estratégica e impactante. Minha agência de marketing não só cria estratégias, mas transforma negócios com soluções personalizadas que realmente fazem a diferença!\n\nSou formada em Marketing pela Uniritter e completamente apaixonada pelo que faço! Curiosa por natureza, estou sempre aprendendo e me atualizando para entregar o melhor aos meus clientes.\n\nHoje, já levamos nossos resultados para diversas cidades, como Porto Alegre, Viamão, Florianópolis, Balneário Camboriú e até Orlando, nos EUA! E agora, estamos prontos para levar a sua empresa para o próximo nível também.\n\nSe você quer ver seu negócio crescer no digital, está no lugar certo. Vamos juntas dar o próximo passo para virar a chave do seu negócio? 🚀💡";
@@ -16,7 +19,11 @@ const WhoWeAreSession = () => {
     </Button>;
 
     return (
-        <div className="flex flex-col w-full">
+        <motion.div 
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1, transition: { duration: 1 } }}
+            viewport={{ once: true }}
+            className="flex flex-col w-full">
             <div className="flex flex-col items-center pb-6">
                 <h2 className="text-lg uppercase font-bold text-primary">🙋🏼‍♀️ Quem sou eu 🙋🏼‍♀️</h2>
                 <p className="text-sm text-muted-foreground">Um pouco da minha história</p>
@@ -43,7 +50,7 @@ const WhoWeAreSession = () => {
                     </div>
                 </div>
             </div>
-        </div>
+        </motion.div>
     );
 }
 

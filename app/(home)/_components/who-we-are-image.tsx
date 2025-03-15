a579f1990ca59f42d51106465e68057e081e2aa6
Davi Santos <davisantos448@gmail.com>
2025-03-15 14:20:32 -0300

    refactor(WhoWeAreImage): change next/image to img native element.

--- app/(home)/_components/who-we-are-image.tsx
+++ app/(home)/_components/who-we-are-image.tsx
@@ -1,14 +1,12 @@
-import Image from "next/image";
-
 interface WhoWeAreImageParameters {
     imagePath: string,
 }
 
 const WhoWeAreImage = ({ imagePath }: WhoWeAreImageParameters) => {
     return (
-        <Image
+        <img
             src={imagePath}
-            alt="quem-sou-1.JPEG"
+            alt="Deh do Marketing Image"
             width="100" height="300"
             className="
                 rounded-xl object-cover 
